2a785c83f090d52c0519f9279e14c0e78a02a059
Krilline <krillinee@gmail.com>
2020-04-03 13:03:34 +0200

    completed php and a part of css

--- public/book.php
+++ public/book.php
@@ -9,7 +9,14 @@
 </head>
 <body>
 
-<?php include 'header.php'; ?>
+<?php include 'header.php';
+require_once 'connec.php';
+$pdo = new PDO(DSN, USER, PASS);
+if($pdo===false){
+    echo 'connection error';
+}
+
+?>
 
 <main class="container">
 
@@ -21,10 +28,80 @@
             <div class="page leftpage">
                 Add a bribe
                 <!-- TODO : Form -->
+
+                <?php 
+                $sql = "INSERT INTO bribe (name, payment) VALUES (:name, :payment)";
+
+                if($_SERVER['REQUEST_METHOD'] === 'POST'){
+                    $error = [];
+                    if(empty($_POST['name'])){
+                        $error['name'] = "Name is missing";
+                    }if(empty($_POST['payment']) || $_POST['payment']<0){
+                        $error['payment'] = "Payment is missing or < 0";
+                    }if(!empty($_POST['name']) && !empty($_POST['payment'] && $_POST['payment']>0)){
+                        
+                        $prepared = $pdo->prepare($sql);
+
+                        $prepared->bindValue(':name', $_POST['name'], PDO::PARAM_STR);
+                        $prepared->bindValue(':payment', $_POST['payment'], PDO::PARAM_STR);
+
+                        $prepared->execute();
+                        Header('Location: http://localhost:8000/index.php');
+                    }
+                }
+                
+                ?>
+
+                <form method="post">
+                    <label for="name">Enter Name :</label>
+                    <code style="color: red"><?php if(isset($error['name'])) echo $error['name'];?></code>
+                    <input type="text" id="name" name="name">
+                    <label for="payment">Payment :</label>
+                    <code style="color: red"><?php if(isset($error['payment'])) echo $error['payment'];?></code>
+                    <input type="text" id="payment" name="payment">
+                    <br>
+                    <button>Pay !</button>
+                </form>
             </div>
 
             <div class="page rightpage">
                 <!-- TODO : Display bribes and total paiement -->
+
+                <?php
+
+                $sql = "SELECT * FROM bribe ORDER BY name";
+                $results = $pdo->query($sql);
+                $bribes = $results->fetchAll(PDO::FETCH_ASSOC);
+                ?>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>NAME</th>
+                        <th>PAYMENT</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                  <?php
+                    foreach($bribes as $bribe){
+                        ?>
+                    <tr>
+                        <td><?= $bribe['name'];?></td>
+                        <td><?= $bribe['payment'];?></td>
+                    </tr>
+                    <?php } ?>
+                    <tfoot>
+                        <?php
+                        
+                        $sql = "SELECT SUM(payment) AS total FROM bribe";
+                        $result = $pdo->query($sql);
+                        $result = $result->fetch();
+                        $total = $result['total']
+                        ?>
+                        <td></td>
+                        <td><?= "Total : ". $total;?></td>
+                    </tfoot>
+                    </tbody>
+                </table>
             </div>
         </div>
         <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
