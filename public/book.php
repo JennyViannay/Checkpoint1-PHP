c75b7c376171ff9ee3103b4f447b5605a8aff833
Nick-LGD <n.legrand77190@gmail.com>
2020-04-03 11:52:39 +0200

    add form and tb

--- public/book.php
+++ public/book.php
@@ -21,10 +21,144 @@
             <div class="page leftpage">
                 Add a bribe
                 <!-- TODO : Form -->
+
+                <?php
+
+
+                $pdo = new \PDO(DSN, USER, PASS);
+                $query = "SELECT * FROM bride";
+                $statement = $pdo->query($query);
+                $bride = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+                $name = '';
+                $payment = '';
+                $nameErr ='';
+                $paymentErr = '';
+                $error = 0;
+
+                function test_input($data) {
+                    $data = trim($data);
+                    $data = stripslashes($data);
+                    $data = htmlspecialchars($data);
+                    return $data;
+                }
+
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+
+                    if (empty($_POST['name'])) {
+                        $nameErr = '* Name required';
+                        $error += 1;
+                    } else {
+                        $name = test_input($_POST['name']);
+                        if (!preg_match("/^([\p{L}a-zA-Z ]*)$/ui", $name)  || strlen($name) > 45) {
+                            $nameErr = '* Only letters and white spaces';
+                            $error += 1;
+                        }
+                    }
+
+                    if (empty($_POST['payment'])) {
+                        $paymentErr = '* payment required';
+                        $error += 1;
+                    } else {
+                        $payment = test_input($_POST['firstname']);
+                        if (!preg_match("#^(1|2|3|4|5|6|7|8|9|10)$#", $payment) || strlen($payment) > 45) {
+                            $paymentErr = '* Only numbers';
+                            $error += 1;
+                        }
+                    }
+
+
+                    if ($error === 0) {
+                        $query = 'INSERT INTO bride (name, payment) VALUES (:name, :payment)';
+                        $statement = $pdo->prepare($query);
+
+                        $statement->bindValue(':name', $name, \PDO::PARAM_STR);
+                        $statement->bindValue(':payment', $payment, \PDO::PARAM_STR);
+
+                        $statement->execute();
+                    }
+                }
+                ?>
+
+
+                <form  action="index.php"  method="post">
+                    <div>
+                        <label  for="name">Name :</label>
+                        <input  type="text"  id="name"  name="name">
+                        <span class="error"><?php echo $nameErr;?></span>
+                        <br><br>
+                    </div>
+                    <div>
+                        <label  for="payment">Payment :</label>
+                        <input  type="text"  id="payment"  name="payment">
+                        <span class="error"><?php echo $paymentErr;?></span>
+                        <br><br>
+                    </div>
+                    <div  class="button">
+                        <button  type="submit">Add</button>
+                    </div>
+                </form>
+
+
+
             </div>
 
             <div class="page rightpage">
                 <!-- TODO : Display bribes and total paiement -->
+                 <?php
+                
+
+                $pdo = new \PDO(DSN, USER, PASS);
+                $query = "SELECT * FROM bride";
+                
+                try {
+                    $pdo_select = $pdo->prepare($query);
+                    $pdo_select->execute();
+                    $NbreData = $pdo_select->rowCount();   
+                    $rowAll = $pdo_select->fetchAll();
+                  } catch (PDOException $e){ echo 'Erreur SQL : '. $e->getMessage().'<br/>'; die(); }
+               
+                
+                if ($NbreData != 0) 
+                {
+                ?>
+                <tfoot>
+                    <table border="1">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>name</th>
+                            <th>payement</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                <?php
+                 
+                    foreach ( $rowAll as $row ) 
+                    {
+                        
+                ?>
+                        <tr>
+                            <td><?php echo $row['id']; ?></td>
+                            <td><?php echo $row['name']; ?></td>
+                            <td><?php echo $row['payment']; ?></td>
+                        </tr>
+                <?php
+                    } // fin foreach
+                ?>
+                    </tbody>
+                    </table>
+                <?php
+                } else { ?>
+                  
+                <?php
+                }
+                ?>
+            </tfoot>
+
+            
             </div>
         </div>
         <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
