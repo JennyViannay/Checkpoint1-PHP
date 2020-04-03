45f36a31fbd3523a249a2508db8cb1defa0dcb95
anaisalfonsi <alfonsi_anais@hotmail.fr>
2020-04-03 10:34:57 +0200

    first attempt to push

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/instructions.css">
-    <title>Checkpoint PHP 1</title>
+    <title>yoCheckpoint PHP 1</title>
 </head>
 <body>
 
