bb6f980067583a9de5c30a2c9e85d49bba6e343f
Guillaume <gdv.delvecchio75@gmail.com>
2020-04-03 10:49:04 +0200

    add form and response system

--- public/book.php
+++ public/book.php
@@ -1,3 +1,14 @@
+<?php 
+
+$dsn="mysql:host=localhost;dbname=checkpoint1";
+$user="root";
+$password="root";
+
+$pdo= new PDO($dsn,$user,$password);
+
+?>
+
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,6 +22,25 @@
 
 <?php include 'header.php'; ?>
 
+<?php 
+$pdo= new PDO($dsn,$user,$password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+if (isset($_POST)){
+    var_dump($_POST);
+    if(!empty($_POST['name']) && !empty($_POST['payment'])){
+        $insertFriends = $pdo->prepare("INSERT INTO bribe(name, payment) VALUES (:name, :payment)");
+        $insertFriends->execute([
+            'name'=> $_POST['name'],
+            'payment'=> $_POST['payment'],
+            ]);
+        
+    }else{
+        echo 'veuillez remplir tous les champs';
+        
+    }
+}
+
+
+?>
 <main class="container">
 
     <section class="desktop">
@@ -21,10 +51,39 @@
             <div class="page leftpage">
                 Add a bribe
                 <!-- TODO : Form -->
+                <form method="POST">
+<label for="name"></label>
+<input type="text" name="name" id="name" placeholder="Name">
+<br>
+<label for="payment"></label>
+<input type="text" name="payment" id="payment" placeholder="Payment">
+<br>
+<button type="submit"> Create</button>
+</form>
             </div>
 
             <div class="page rightpage">
                 <!-- TODO : Display bribes and total paiement -->
+<?php
+if ($pdo === false){
+    echo "error connexion".$pdo->error_log();
+}else{
+    $query = "SELECT * FROM bribe";
+    $statement = $pdo->query($query);   
+    $bribe = $statement->fetchAll(PDO::FETCH_ASSOC);
+    //var_dump($brib);
+    foreach($bribe as $brib){
+        //var_dump($brib);
+?>
+                <div>
+                    <p> <?php echo $brib['name'];?></p>
+                    <p> <?php echo $brib['payment'];?></p>
+                </div>
+<?php
+    }
+}
+
+?>
             </div>
         </div>
         <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
