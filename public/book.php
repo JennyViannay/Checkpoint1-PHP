254499882973f9ad9bb22ed28796e3d4b21c8ecb
Jules DUMAS <jules_ox@hotmail.fr>
2020-04-03 12:59:14 +0200

    first commit

--- public/book.php
+++ public/book.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'connec.php';
+$pdo = new PDO(DSN,USER,PASS);
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -19,12 +23,63 @@
 
         <div class="pages">
             <div class="page leftpage">
-                Add a bribe
-                <!-- TODO : Form -->
+            <?php
+            if($_POST){
+                $name = trim($_POST['name']);
+                $payment = trim($_POST['payment']);
+                $query = "INSERT INTO bride (name, payment) VALUES (':nom', ':pay')";
+                $statement = $pdo->prepare($query);
+                $statement->bindValue(':nom', $name, PDO::PARAM_STR);
+                $statement->bindValue(':pay', $payment, PDO::PARAM_INT);
+                $statement->execute();
+                }
+                ?>
+                <form  action=""  method="POST">
+                    <div>
+                        <label  for="nom">name :</label>
+                        <input  type="text"  id="nom"  name="name" required >
+                    </div>
+                    <div>
+                        <label  for="pay">payment :</label>
+                        <input  type="text"  id="pay"  name="payment" required>
+                    </div>
+                    <button>submit</button>
+                </form>
             </div>
-
             <div class="page rightpage">
-                <!-- TODO : Display bribes and total paiement -->
+            <?php
+
+            $query = "SELECT * FROM bride ORDER BY name ASC";
+            $statement = $pdo->prepare($query);
+            $statement->execute();
+            $NbData = $statement->rowCount();
+            $brides = $statement->fetchALL();
+            $total = 0;
+            foreach ($brides as $bride){
+                $total += $bride['payment'];
+            }
+            foreach($brides as $bride)
+            {
+            ?>
+            <table>
+            <thead>
+            <tr>
+                <td><?php echo $bride['id']; ?></td>
+                <td><?php echo $bride['name']; ?></td>
+                <td><?php echo $bride['payment']; ?></td>
+            </tr>
+            </thead>
+            <?php
+              } 
+            ?>
+            <tbody>
+                <tr>
+                    <td> TOTAL : <?php echo $total?></td>
+                </tr>
+                </tbody>
+            </table>
+            </tbody>
+            </table>
             </div>
         </div>
         <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
